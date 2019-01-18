31764d24a28d09b8b9a05b48303e9c180be67499
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-18 21:02:15 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+def using_push(push_array, push_string)
+  push_array.push(push_string)
+end
+
+def using_unshift(unshift_array, unshift_string)
+  unshift_array.unshift(unshift_string)
+end
+
+def using_pop(pop_array)
+  pop_array.pop
+end
+
+def pop_with_args(pop_arg_array)
+  pop_arg_array.pop(2)
+end
